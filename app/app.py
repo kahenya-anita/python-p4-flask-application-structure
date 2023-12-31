7c0bd43f574f297316852d0ecbb7343813ec0048
anita-that-chiq <62019551+kahenya-anita@users.noreply.github.com>
2023-12-31 15:18:06 +0300

    Final changes

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my app!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
